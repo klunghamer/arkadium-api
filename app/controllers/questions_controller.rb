a385f1e0c787c989f2919fdd81cb21a89646cb46
Kelly Lunghamer <klung@umich.edu>
2016-12-09 12:35:36 -0500

    Changed API key

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
 
   def number
     number = params[:number].to_i
-    resource = RestClient.get "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['API_KEY']}&with_people=287"
+    resource = RestClient.get "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_KEY']}&with_people=287"
     result = JSON.parse(resource.body)
     question = result['results'][number]
     render json: {question: question}
